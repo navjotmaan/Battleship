7d2a2696b4bff4d5f2c84d9d4d5d78b3ccf8d04f
navjotmaan <navjotmaan137@gmail.com>
2025-11-20 02:12:04 +0000

    add more tests for placeShip function

--- index.test.js
+++ index.test.js
@@ -19,7 +19,7 @@ describe('Ship', () => {
 });
 
 describe('Gameboard', () => {
-    test('placed the ship inside the gameboard', () => {
+    test('places ship horizontally', () => {
         const board = new Gameboard();
         const ship = new Ship(3);
 
@@ -28,5 +28,39 @@ describe('Gameboard', () => {
         expect(board.board[3][5]).toBe(ship);
         expect(board.board[3][6]).toBe(ship);
         expect(board.board[3][7]).toBe(ship);
+
     });
+
+    test('rejects ship placement out of bounds', () => {
+        const board = new Gameboard();
+        const ship = new Ship(3); 
+
+        const result = board.placeShip(3, 8, ship);
+        expect(result).toBe('invalid coordinates');
+    });
+
+    test('rejects overlapping ship placement', () => {
+        const board = new Gameboard();
+        const ship1 = new Ship(3);
+        const ship2 = new Ship(3);
+
+        board.placeShip(3, 5, ship1);
+        const result = board.placeShip(3, 5, ship2);
+
+        expect(result).toBe('already occupied');
+    });
+
+    test('stores ship coordinates inside ship.position', () => {
+        const board = new Gameboard();
+        const ship = new Ship(3);
+
+        board.placeShip(3, 5, ship);
+
+        expect(ship.position).toEqual([
+            [3, 5],
+            [3, 6],
+            [3, 7]
+        ]);
+    });
+
 });
