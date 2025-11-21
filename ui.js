4c8fa90ea0bbaa1c6f274b9bcc64c35be7ac7237
navjotmaan <navjotmaan137@gmail.com>
2025-11-21 13:28:47 +0000

    created gameboard using createGrid function and add an event listener for cells

--- ui.js
+++ ui.js
@@ -1,1 +1,28 @@
-const { Ship, Gameboard } = require('./index');
+import { Ship, Gameboard } from './script.js';
+
+const playerBoardDiv = document.getElementById("player-board");
+
+function createGrid(boardDiv) {
+    for (let i = 0; i < 100; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.dataset.x = Math.floor(i / 10);
+        cell.dataset.y = i % 10;
+        boardDiv.appendChild(cell);
+    }
+}
+
+createGrid(playerBoardDiv);
+
+const game = new Gameboard();
+
+playerBoardDiv.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("cell")) return;
+    
+    let x = Number(e.target.dataset.x);
+    let y = Number(e.target.dataset.y);
+
+    console.log(`Clicked: ${x}, ${y}`);
+});
+
+
